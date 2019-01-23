55ee9f4bd8710750b30ad354fd0bae4a4a67707d
Marcrobito <marco.ant.mtz@gmail.com>
2019-01-23 12:56:33 -0600

    Ios configuration

---
+++ whisper/whisper/main/MainPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MainPresenter.swift
+//  whisper
+//
+//  Created by Marco Antonio Martínez Gutiérrez on 23/01/19.
+//  Copyright © 2019 Marco Antonio Martínez Gutiérrez. All rights reserved.
+//
+
+import Foundation
